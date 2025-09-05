60f77e5cb3a30c12c3600701d438d53db71d680b
Martin Lang <martin.lang@mpsd.mpg.de>
2025-09-05 17:35:45 +0200

    Update package

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,7 @@
-cmake -S . -B _build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=ON ${CMAKE_ARGS}
+set -euxo pipefail
+umask 022
+
+cmake -S . -B _build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=ON ${CMAKE_ARGS} -DUSE_OPENMP:BOOL=ON -DUSE_FFTW:BOOL=ON -DUSE_MKL:BOOL=OFF
 cmake --build _build
 cmake --install _build --prefix $PREFIX
-
+rm -f "${PREFIX}/lib/libasdlib.a"
